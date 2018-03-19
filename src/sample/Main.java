be5018d7763aaef981123051e9a04a2bb33588d7
Nikhil Palli <npalli1@student.gsu.edu>
2018-03-19 12:08:33 -0400

    Test

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,40 +13,43 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.sql.ResultSet;
+
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
 
         Label welcomeLabel = new Label ( "Welcome" );
         welcomeLabel.setStyle ( "-fx-font-size: 24pt" );
-        Label userIdLabel = new Label ("User Id: ");
+        Label userIdLabel = new Label ( "User Id: " );
         TextField userIdTextField = new TextField ();
-        Label pinLabel = new Label("Pin: ");
+        Label pinLabel = new Label ( "Pin: " );
         TextField pinTextField = new TextField ();
         Button login = new Button ( "Login" );
         Button forgotPin = new Button ( "Forgot Password" );
 
         GridPane welcomeGrid = new GridPane ();
 
-        GridPane.setConstraints ( welcomeLabel,0,0 );
-        GridPane.setConstraints ( userIdLabel,0, 1);
-        GridPane.setConstraints ( userIdTextField,1, 1);
-        GridPane.setConstraints ( pinLabel,0, 2);
-        GridPane.setConstraints ( pinTextField ,1,2);
-        GridPane.setConstraints ( login,0, 3);
-        GridPane.setConstraints ( forgotPin ,1, 3);
+        GridPane.setConstraints ( welcomeLabel, 0, 0 );
+        GridPane.setConstraints ( userIdLabel, 0, 1 );
+        GridPane.setConstraints ( userIdTextField, 1, 1 );
+        GridPane.setConstraints ( pinLabel, 0, 2 );
+        GridPane.setConstraints ( pinTextField, 1, 2 );
+        GridPane.setConstraints ( login, 0, 3 );
+        GridPane.setConstraints ( forgotPin, 1, 3 );
 
-        welcomeGrid.getChildren ().addAll(welcomeLabel,userIdLabel,userIdTextField,pinLabel,pinTextField,login,forgotPin);
+        welcomeGrid.getChildren ().addAll ( welcomeLabel, userIdLabel, userIdTextField, pinLabel, pinTextField, login, forgotPin );
         welcomeGrid.setVgap ( 25 );
         welcomeGrid.setHgap ( 25 );
         welcomeGrid.setPadding ( new Insets ( 50 ) );
         welcomeGrid.setAlignment ( Pos.CENTER );
 
-        Scene start = new Scene (welcomeGrid,1600,1000);
-        primaryStage.setTitle("P&N Enterprise");
-        primaryStage.setScene(start);
-        primaryStage.show();
+        Scene start = new Scene ( welcomeGrid, 1600, 1000 );
+        primaryStage.setTitle ( "P&N Enterprise" );
+        primaryStage.setScene ( start );
+        primaryStage.show ();
+
 
     }
 
