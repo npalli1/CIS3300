24077e00807223c7311dd928937980c7806e1f0b
Nikhil Palli <npalli1@student.gsu.edu>
2018-04-04 17:25:32 -0400

    Created Part class

---
+++ src/Backend/Part.java
@@ -0,0 +1,46 @@
+package Backend;
+
+/**
+ * Created by nikhilpalli on 4/4/18.
+ */
+public class Part {
+
+    int partId;
+    String partLocation;
+    String partName;
+    String price;
+
+    public int getPartId() {
+        return partId;
+    }
+
+    public void setPartId(int partId) {
+        this.partId = partId;
+    }
+
+    public String getPartLocation() {
+        return partLocation;
+    }
+
+    public void setPartLocation(String partLocation) {
+        this.partLocation = partLocation;
+    }
+
+    public String getPartName() {
+        return partName;
+    }
+
+    public void setPartName(String partName) {
+        this.partName = partName;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
+
+
